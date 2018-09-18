2154d6ff7aea87287e26e9a02394c5a77ddcd40c
NaNSix <243595837@qq.com>
2018-09-18 11:52:53 +0800

    礼包页面的逻辑

--- KekeWeb/js/pageJs/gifts.js
+++ KekeWeb/js/pageJs/gifts.js
@@ -22,18 +22,14 @@ if (sessionStorage.loginSuccess && sessionStorage.loginSuccess === 'success') {
         axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
         // todo 去除滚动条
-        const [giftListUrl, getCDkeyRUL, now_time, platform, ] = ['/gift-bag/index', '/gift-bag-user-cdkey/index', parseInt(moment().unix()), 'web', ]
-        let [android_download_url, describe, game_id, hot, ios_download_url, logo, name, imgBoxHTMLList, downLoadHTML, hotHTML, divList] = [
+        const [giftListUrl, CDKeydURL, now_time, platform, ] = ['/gift-bag/index', '/gift-bag-user-cdkey/index', parseInt(moment().unix()), 'web', ]
+        let [describe, game_name, gift_bag_id, gift_bag_name, hot, CDkeyHTML] = [
             [],
             [],
             [],
             [],
             [],
             [],
-            [],
-            [],
-            [],
-            [], ''
         ]
         const signObj = {
             now_time,
@@ -49,50 +45,88 @@ if (sessionStorage.loginSuccess && sessionStorage.loginSuccess === 'success') {
             },
         }
 
-        function getgiftListUrl() {
-            return axios.get(giftListUrl, axiosConfig);
-        }
+        axios(giftListUrl, axiosConfig)
+            .then(function(response) {
+                let data = response.data.data
+                HTMLAdd(data)
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
 
-
-        function getCDkey() {
-            return axios.get(getCDkeyRUL, axiosConfig);
-        }
-
-
-        axios.all([getgiftListUrl(), getCDkey()])
-            .then(axios.spread(function(giftListURL, CDKey) {
-                if (giftListURL.data.data) {
-                    console.log(giftListURL.data.data)
-                        // HTMLAdd(giftListURL.data.data, index = 0)
-                }
-                if (CDKey.data.data) {
-                    console.log(giftListURL.data.data)
-
-                    // HTMLAdd(CDKey.data.data, index = 1)
-                }
-            }))
 
         function HTMLAdd(data, index) {
-            data = _.sortBy(data, function(item) {
-                return item.hot;
-            });
+            _.sortBy(data, (data) => {
+                return data.hot
+            })
+            data.map((item, index) => {
+                describe.push(item.describe)
+                game_name.push(item.game_name)
+                gift_bag_id.push(item.gift_bag_id)
+                gift_bag_name.push(item.gift_bag_name)
+                hot.push(item.hot)
+            })
+
             HTMLCreate(data, index)
-            if (index === 0) {
+            $(".giftBox").html(CDkeyHTML)
 
-                $(".downLoad").html(downLoadHTML)
 
-                //模态框加载数据
-                divList = $("div[data-indexNEW]")
-            } else if (index === 1) {
-                $("#tb2").html(hotHTML)
 
-                //模态框加载数据
-                divList = $("div[data-indexHot]")
+            //礼包领取
+            CDKey(gift_bag_id)
+
+        }
+
+        //礼包领取
+        function CDKey(gift_bag_id) {
+            if (gift_bag_id && !!gift_bag_id) {
+                const giftList = document.querySelectorAll('.gift_bag_id')
+                for (let i = 0; i < giftList.length; i++) {
+                    giftList[i].addEventListener('click', () => {
+                        Object.assign(signObj, { gift_bag_id: gift_bag_id[i] })
+                        let sign = getSign(signObj) //调用签名函数获取签名
+                        let axiosConfig = {
+                            method: "GET",
+                            params: {
+                                now_time,
+                                platform,
+                                sign,
+                                gift_bag_id: gift_bag_id[i]
+                            },
+                        }
+                        axios(CDKeydURL, axiosConfig)
+                            .then(function(response) {
+                                console.log(response);
+                                //模态框加载数据
+                                modalStart("div[data-index]")
+                            })
+                            .catch(function(error) {
+                                console.log(error);
+                            });
+                    }, false)
+                }
+
+            } else {
+                // layer提示
+                mui("#tb1").on('tap', '.gf', function(event) {
+                    layer.open({
+                        content: '暂无礼包领取',
+                        skin: 'msg',
+                        time: 2 //2秒后自动关闭
+                    });
+
+                });
             }
+        }
+
+        function modalStart(NodeList) {
+            //模态框加载数据
+            let divList = $(NodeList)
             for (let i = 0; i < divList.length; i++) {
                 divList[i].onclick = function() {
-                    let params = [logo[i], name[i], describe[i], android_download_url[i], ios_download_url[i]]
-                    let modalHTML = modal(params)
+                    let params = [describe[i], game_name[i]]
+                    console.log(divList[i])
+                    let modalHTML = modal(params, 0)
                     $('#modal').html(modalHTML)
                 }
             }
@@ -100,70 +134,29 @@ if (sessionStorage.loginSuccess && sessionStorage.loginSuccess === 'success') {
 
         function HTMLCreate(data, index) {
             for (let i = 0; i < data.length; i++) {
-                android_download_url.push(data[i].android_download_url)
-                describe.push(data[i].describe)
-                game_id.push(data[i].game_id)
-                hot.push(data[i].hot)
-                ios_download_url.push(data[i].ios_download_url)
-                logo.push(data[i].logo)
-                name.push(data[i].name)
-                if (logo.length !== 1) {
-                    imgBoxHTMLList.push(`<img src=${data[i].logo} />`)
-                } else {
-
-                }
-                if (index === 0) {
-                    downLoadHTML += `
-                        <div class="content" data-indexNEW=${i}>
-                        <div class="middle" style="width: 77%;margin-left: 3%;">
-                            <div class="left_img">
-                                <img src=${data[i].logo} style="width: 90%;" />
-                            </div>
-                            <div class="right_info">
-                                <p class="d_name">${data[i].name}</p>
-                                <p style="line-height: 1.1rem;">
-                                    <img src="images/angle.svg" />
-                                    <img src="images/angle.svg" />
-                                    <img src="images/angle.svg" />
-                                    <img src="images/angle.svg" />
-                                    <img src="images/angle.svg" />
-                                </p>
-                                <p class="d_describe">${data[i].describe}</p>
-                            </div>
+                CDkeyHTML += `
+                <div class="content" data-index=${i}>
+                    <div class="middle" style="width: 69%;margin-left: 4%;">
+                        <div class="left_img">
+                            <img src="images/icon.png" class="imagess" />
                         </div>
-                        <div class="right">
-                            <button class="dlBtn" data-toggle="modal" data-target="#downModal">下载</button>
+                        <div class="right_info">
+                            <p class="d_name">${data[i].game_name}</p>
+                            <p style="line-height: 1.1rem;">
+                                <img src="images/angle.svg" />
+                                <img src="images/angle.svg" />
+                                <img src="images/angle.svg" />
+                                <img src="images/angle.svg" />
+                                <img src="images/angle.svg" />
+                            </p>
+                            <p class="d_describe">${data[i].describe}</p>
                         </div>
                     </div>
-                        `
-                } else {
-                    hotHTML += `
-                    <div class="content" data-indexHot=${i}>
-                        <div class="left">
-                            <img src=${data[i].logo} />
-                        </div>
-                        <div class="middle">
-                            <div class="left_img">
-                                <img src="images/icon.png" style="width: 90%;" />
-                            </div>
-                            <div class="right_info">
-                                <p class="d_name">${data[i].name}</p>
-                                <p style="line-height: 1.1rem;">
-                                    <img src="images/angle.svg" />
-                                    <img src="images/angle.svg" />
-                                    <img src="images/angle.svg" />
-                                    <img src="images/angle.svg" />
-                                    <img src="images/angle.svg" />
-                                </p>
-                                <p class="d_describe">${data[i].describe}</p>
-                            </div>
-                        </div>
-                        <div class="right">
-                            <button class="dlBtn" data-toggle="modal" data-target="#downModal">下载</button>
-                        </div>
+                    <div class="right">
+                        <button class="dlBtn gf gift_bag_id" data-toggle="modal" data-target="#downModal">领取礼包</button>
                     </div>
-                        `
-                }
+                </div>
+                    `
             }
         }
 
