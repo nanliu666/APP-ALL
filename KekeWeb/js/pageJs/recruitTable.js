7b9cc9a5bb321fc58effc6f7e50772642f802f7d
NaNSix <243595837@qq.com>
2018-09-20 17:31:15 +0800

    完成招聘列表

--- KekeWeb/js/pageJs/recruitTable.js
+++ KekeWeb/js/pageJs/recruitTable.js
@@ -1,126 +1,117 @@
-if (sessionStorage.loginSuccess && sessionStorage.loginSuccess === 'success') {
-    $(document).ready(() => {
-        if (sessionStorage && sessionStorage.recruit === 'school') {
-            document.title = "校园招聘"
-        } else {
-            document.title = "社会招聘"
-        }
-        const [newSortUrl, recommendURL, ] = ['/recruit/type', '/recruit/position']
-
-        // axios GET参数获取
-        const axiosConfig = $axiosGetConfig()
-
-        axios(newSortUrl, axiosConfig)
-            .then((result) => {
-                console.log(result)
-            })
-            .catch((err) => {
-                console.log(err)
-            })
-
-        axios(recommendURL, axiosConfig)
-            .then((result) => {
-                console.log(result)
-            })
-            .catch((err) => {
-                console.log(err)
-            })
-
-        const reciuitText = ['全部', '技术类', '策划类', '美术类', '职能类', ]
-        const tBodyText = [{
-            A: 'Erlang服务端开发工程师111',
-            B: '技术类',
-            C: '面议'
-        }, {
-            A: 'Erlang服务端开发工程师222',
-            B: '策划类',
-            C: '面议'
-        }, {
-            A: 'Erlang服务端开发工程师333',
-            B: '美术类',
-            C: '面议'
-        }, , {
-            A: 'Erlang服务端开发工程师444',
-            B: '职能类',
-            C: '面议'
-        }, , {
-            A: 'Erlang服务端开发工程师555',
-            B: '技术类',
-            C: '面议'
-        }, ]
-        let [headerHTML, LiHTML, tBody1, tBody2, tBody3, tBody4, tBody5, ] = ['', [],
-            [],
-        ]
-        headerHTML = `
-        <div class="topInfo">
-        <a href='./index.html'><img src="images/logo (2).png" class="logo" /></a>
-        <span style="float: right">
-                    <span class="mui-icon mui-icon-contact" id="JSCenter" style="font-size:38px; padding: 10px"></span>
-        </span>
-    </div>
-        `
-
-        for (let i = 0; i < reciuitText.length; i++) {
-            LiHTML.push(`
-            <li class="flexCenterW div25" data-index='${i}'>${reciuitText[i]}</li>
-        `)
-        }
-        LiHTML[0] = `<li class="div25 flexCenterW  active" data-index='${0}'>${reciuitText[0]}</li>`
-
-        $('header').html(headerHTML)
-        $('nav').html(LiHTML)
-
-        tBodyText.map((item, index, tBodyText) => {
-            if (item.B === '技术类') {
-                tBody2 += `
-        <tr><td>${item.A}</td><td>${item.B}</td><td>${item.C}</td></tr>
-    `
-            }
-            if (item.B === '策划类') {
-                tBody3 += `
-        <tr><td>${item.A}</td><td>${item.B}</td><td>${item.C}</td></tr>
-    `
-            }
-            if (item.B === '美术类') {
-                tBody4 += `
-        <tr><td>${item.A}</td><td>${item.B}</td><td>${item.C}</td></tr>
-    `
-            }
-            if (item.B === '职能类') {
-                tBody5 += `
-        <tr><td>${item.A}</td><td>${item.B}</td><td>${item.C}</td></tr>
-    `
+const recruitTableFun = (() => {
+    if (sessionStorage.loginSuccess && sessionStorage.loginSuccess === 'success') {
+        $(document).ready(() => {
+            if (sessionStorage && sessionStorage.recruit === 'school') {
+                document.title = "校园招聘"
             } else {
-                tBody1 += `
-            <tr><td>${item.A}</td><td>${item.B}</td><td>${item.C}</td></tr>
-        `
+                document.title = "社会招聘"
             }
-        })
-        $('tbody').html(tBody1)
-        $("nav li").click(function() {
-            var item = $(this).index(); //获取索引下标 也从0开始
-            $(this).siblings().removeClass('active')
-            $(this).addClass('active')
 
-            switch ($(this)[0].innerText) {
-                case '技术类':
-                    $('tbody').html(tBody2)
-                    break
-                case '策划类':
-                    $('tbody').html(tBody3)
-                    break
-                case '美术类':
-                    $('tbody').html(tBody4)
-                    break
-                case '职能类':
-                    $('tbody').html(tBody5)
-                    break
-                default:
-                    $('tbody').html(tBody1)
-                    break
+            // URL统一管理
+            const [recruitTypeUrl, recruitPositionURL, ] = ['/recruit/type', '/recruit/position']
+
+            //传入数据统一管理
+            let [typeData, positionData] = [
+                [],
+                []
+            ]
+
+
+            // 拼接字符串统一管理
+            let [headerHTML, LiHTML, TbodyAddHTML, ] = ['', [],
+                [],
+            ]
+
+            const navText = ['全部', '技术类', '策划类', '美术类', '职能类', ]
+
+            // axios GET参数获取
+            let axiosConfig = $axiosGetConfig()
+
+            const navAxios = (() => {
+                axios(recruitTypeUrl, axiosConfig)
+                    .then((result) => {
+                        typeData = result.data.data
+                        navLi(typeData)
+                    })
+                    .catch((err) => {
+                        console.log(err)
+                    })
+            })()
+
+            const TBodyAxios = ((recruit_type_id) => {
+                if (!!recruit_type_id) {
+                    axiosConfig = $axiosGetConfig({ 'recruit_type_id': recruit_type_id })
+                } else { //直接请求所有岗位
+                    axiosConfig = axiosConfig
+                }
+                axios(recruitPositionURL, axiosConfig)
+                    .then((result) => {
+                        positionData = Array.from(new Set(result.data.data))
+                        console.log(positionData)
+                        TbodyAdd(positionData)
+                    })
+                    .catch((err) => {
+                        console.log(err)
+                    })
+            })
+
+            // 拉取全部职位请求
+            TBodyAxios()
+
+            const headerHTMLAdd = (() => {
+                headerHTML = `
+                <div class="topInfo">
+                    <a href='./index.html'><img src="images/logo (2).png" class="logo" /></a>
+                    <span style="float: right">
+                                <span class="mui-icon mui-icon-contact" id="JSCenter" style="font-size:38px; padding: 10px"></span>
+                    </span>
+                </div>
+                `
+                $('header').html(headerHTML)
+            })();
+
+            const navLi = ((params) => {
+                LiHTML[0] = `<li class="div25 flexCenterW  active" data-index='${0}'>全部</li>`
+                for (let i = 0; i < params.length; i++) {
+                    LiHTML.push(`
+                    <li class="flexCenterW div25" data-index='${params[i].recruit_type_id}'>${params[i].recruit_type_name}</li>
+                `)
+                }
+                $('nav').html(LiHTML)
+                navLiClick()
+            })
+
+            const TbodyAdd = (params) => {
+                TbodyAddHTML = []
+                params.map((item, index) => {
+                    TbodyAddHTML.push(` <tr recruit_position_id='${item.recruit_position_id}'><td>${item.recruit_position_name}</td><td>${item.recruit_type_name}</td><td>面议</td></tr>`)
+                })
+                $('tbody').html(TbodyAddHTML)
+                TbodyTdClick()
+
             }
+
+            const navLiClick = () => {
+                $("nav li").click(function() {
+                    const recruit_type_id = $(this).attr('data-index')
+                    $(this).siblings().removeClass('active')
+                    $(this).addClass('active')
+                    TBodyAxios(recruit_type_id)
+                })
+            }
+
+            const TbodyTdClick = (() => {
+                $('tbody tr').click(function() { //这里不用箭头函数，this需要指向tbody
+                    console.log($(this).attr('recruit_position_id'))
+                    sessionStorage.setItem('recruit_position_id', $(this).attr('recruit_position_id'))
+                    location.href = './recruitDetail.html'
+                })
+            })
+            $('#JSCenter').on('click', function() {
+                window.location.href = './centerIndex.html'
+            })
         })
-    })
-} else {
-    location.href = './login.html'
-}
+    } else {
+        location.href = './login.html'
+    }
+})()
