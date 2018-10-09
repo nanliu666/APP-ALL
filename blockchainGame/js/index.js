2b5556c7386da3cb6c83a8ed9e14c19390b8f8f7
NaNSix <243595837@qq.com>
2018-10-09 16:01:04 +0800

    将ES6转化成ES5

--- blockchainGame/js/index.js
+++ blockchainGame/js/index.js
@@ -1,117 +1,98 @@
-$(document).ready(() => {
-  const time = moment().format('YYYY-MM-DD HH:mm')
-  const data = [
-    {
-      title: '兑换',
-      gameID: '12324587',
-      balance: 11
-    },
-    {
-      title: '兑换记录',
-      Erecord: [
-        {
-          date: moment().format('YYYY-MM-DD'),
-          number: [
-            {
-              success: 0,
-              time,
-              RMB: 10,
-              gold: 10
-            },
-            {
-              success: 0,
-              time,
-              RMB: 10,
-              gold: 10
-            },
-            {
-              success: 0,
-              time,
-              RMB: 10,
-              gold: 10
-            },
-            {
-              success: 0,
-              time,
-              RMB: 10,
-              gold: 10
-            },
-            {
-              success: 1,
-              time,
-              RMB: 10,
-              gold: 10
-            }
-          ]
-        }
-      ]
-    },
-    {
-      title: '个人中心',
-      gameID: '12324587'
-    }
-  ]
-  let [exchange, record, center] = data
-  let [recordLiHTML, successHTML] = [[], []]
-  document.title = '兑换'
-  $('header h2').text('兑换')
+'use strict';
+
+$(document).ready(function () {
+  var time = moment().format('YYYY-MM-DD HH:mm');
+  var data = [{
+    title: '兑换',
+    gameID: '12324587',
+    balance: 11
+  }, {
+    title: '兑换记录',
+    Erecord: [{
+      date: moment().format('YYYY-MM-DD'),
+      number: [{
+        success: 0,
+        time: time,
+        RMB: 10,
+        gold: 10
+      }, {
+        success: 0,
+        time: time,
+        RMB: 10,
+        gold: 10
+      }, {
+        success: 0,
+        time: time,
+        RMB: 10,
+        gold: 10
+      }, {
+        success: 0,
+        time: time,
+        RMB: 10,
+        gold: 10
+      }, {
+        success: 1,
+        time: time,
+        RMB: 10,
+        gold: 10
+      }]
+    }]
+  }, {
+    title: '个人中心',
+    gameID: '12324587'
+  }];
+  var exchange = data[0],
+      record = data[1],
+      center = data[2];
+  var recordLiHTML = [],
+      successHTML = [];
+
+  document.title = '兑换';
+  $('header h2').text('兑换');
 
   // 记录
-  record.Erecord.map(item => {
-    recordLiHTML.push(`<p>${item.date}</p>`)
-    item.number.map(item => {
-      recordLiHTML.push(`
-                      <li class="padding10">
-                <div class="flexspaceBetween"><span class="fontSize20"><strong>￥${
-                  item.RMB
-                }</strong></span> <span class="jsSuccess textSuccess">${
-        item.success
-      }</span></div>
-                <div class="flexspaceBetween textP">
-                    <span>消耗${item.gold}个区块链币</span>
-                    <span>${item.time}</span>
-                </div>
-                </li>
-            `)
-    })
-  })
-  $('#Js-record ul').html(recordLiHTML)
-  const jsSuccess = $('.jsSuccess')
-  for (let i = 0; i < jsSuccess.length; i++) {
+  record.Erecord.map(function (item) {
+    recordLiHTML.push('<p>' + item.date + '</p>');
+    item.number.map(function (item) {
+      recordLiHTML.push('\n                      <li class="padding10">\n                <div class="flexspaceBetween"><span class="fontSize20"><strong>\uFFE5' + item.RMB + '</strong></span> <span class="jsSuccess textSuccess">' + item.success + '</span></div>\n                <div class="flexspaceBetween textP">\n                    <span>\u6D88\u8017' + item.gold + '\u4E2A\u533A\u5757\u94FE\u5E01</span>\n                    <span>' + item.time + '</span>\n                </div>\n                </li>\n            ');
+    });
+  });
+  $('#Js-record ul').html(recordLiHTML);
+  var jsSuccess = $('.jsSuccess');
+  for (var i = 0; i < jsSuccess.length; i++) {
     if (jsSuccess[i].innerHTML === '0') {
-      jsSuccess[i].innerHTML = '成功'
+      jsSuccess[i].innerHTML = '成功';
     } else {
-      jsSuccess[i].innerHTML = '失败'
-      jsSuccess[i].classList.remove('textSuccess')
-      jsSuccess[i].classList.add('textErr')
+      jsSuccess[i].innerHTML = '失败';
+      jsSuccess[i].classList.remove('textSuccess');
+      jsSuccess[i].classList.add('textErr');
     }
   }
 
   // nav栏切换效果
-  const navList = $('nav li')
-  const mainChildren = $('main').children()
-  for (let i = 0; i < navList.length; i++) {
-    $(navList[i]).click(() => {
-      document.title = data[i].title
-      $('header h2').text(data[i].title)
-      $(navList[i])
-        .addClass('active')
-        .siblings()
-        .removeClass('active')
-      $(mainChildren[i])
-        .show()
-        .siblings()
-        .hide()
-    })
+  var navList = $('nav li');
+  var mainChildren = $('main').children();
+
+  var _loop = function _loop(_i) {
+    $(navList[_i]).click(function () {
+      document.title = data[_i].title;
+      $('header h2').text(data[_i].title);
+      $(navList[_i]).addClass('active').siblings().removeClass('active');
+      $(mainChildren[_i]).show().siblings().hide();
+    });
+  };
+
+  for (var _i = 0; _i < navList.length; _i++) {
+    _loop(_i);
   }
 
   // 跳转修改密码
-  $('.jsPassword').on('click', () => {
-    location.href = './changePassword.html'
-  })
+  $('.jsPassword').on('click', function () {
+    location.href = './changePassword.html';
+  });
 
   // 退出登录
-  $('.loginOut').on('click', () => {
-    location.href = './password.html'
-  })
-})
+  $('.loginOut').on('click', function () {
+    location.href = './password.html';
+  });
+});
