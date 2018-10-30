6d6e9ed683398aa69d3c0606be75d3bb2510c50a
NaNSix <243595837@qq.com>
2018-10-30 19:01:57 +0800

    首页重构初步完成

--- keke/js/index.js
+++ keke/js/index.js
@@ -4,13 +4,9 @@ $(document).ready(function () {
   document.documentElement.style.fontSize = deviceWidth / 6.4 + 'px';
 
 
-  layui.use(['carousel', 'element', 'nav'], function () {
+  layui.use(['carousel', 'element', ], function () {
     var carousel = layui.carousel;
     var element = layui.element;
-    var nav = layui.nav;
-
-    // nav.render(0)
-    console.log(nav)
     //建造实例
     carousel.render({
       elem: '#carousel',
@@ -21,8 +17,6 @@ $(document).ready(function () {
       interval: '3000', //切换动画方式
       arrow: "hover",
     });
-
-
     //获取hash来切换选项卡，假设当前地址的hash为lay-id对应的值
     var layid = location.hash.replace(/^#test1=/, '');
     element.tabChange('test1', layid);
@@ -31,4 +25,5 @@ $(document).ready(function () {
   $('.navbar').on('click', 'li', function () {
     $(this).addClass('actives').siblings().removeClass('actives')
   })
+
 })
